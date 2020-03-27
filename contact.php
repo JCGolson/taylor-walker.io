379fa1bceee467bdabbd71c1aece83180328ba07
jcgolson <juanita.golson@gmail.com>
2020-03-27 11:11:20 -0500

    commit

--- contact.php
+++ contact.php
@@ -1,112 +1,93 @@
-<!--Doctype HTML5 -->
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Taylor-Walker Consulting LLC</title>
-        <meta name="description"
-            content="Taylor-Walker Consulting, LLC is an actuarial consulting firm dedicated to customer service. Our mission is to provide clients with the expertise of a large firm while maintaining the one-on-one ...">
-        <meta name="robots" content="index, follow" />
-        <meta name="copyright" content="2020 - Taylor-Walker" />
+<?php
+/*
+THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+*/
+
+require 'PHPMailerAutoload.php';
+
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
+
+// an email address that will be in the From field of the email.
+$fromEmail = 'webcontact@taylor-walker.com';
+$fromName = 'Demo contact form';
+
+// an email address that will receive the email with the output of the form
+$sendToEmail = 'juanita.golson@gmail.com';
+$sendToName = 'Demo contact form';
+
+// subject of the email
+$subject = 'New message from contact form';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, we will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+*  LET'S DO THE SENDING
+*/
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
     
-        <link href="css/normalize.css" rel="stylesheet">
-        <link href="css/small.css" rel="stylesheet">
-        <link href="css/medium.css" rel="stylesheet">
-        <link href="css/large.css" rel="stylesheet">
-        <link href="css/contact.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Anton&Kanit&Roboto&display=swap" rel="stylesheet">
-    </head>
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
     
-    <body>
+    $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
+    $emailTextHtml .= "<table>";
     
-        <!--------------------------------------Page Header------------------------------->
-        <header class="container">
-            <img class="top-logo" src="images/tw-logo-big.jpg" alt="Taylor-Walker logo">
-            <a href="contact.php"><img class="top-contact" src="images/ContactButton.jpg" alt="Contact Taylor-Walker"></a>
-        </header>
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+        }
+    }
+    $emailTextHtml .= "</table><hr>";
+    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Taylor Walker, LLC</p>";
     
-<!--------------------------------Navigation Bar------------------------------->
-    <nav>
-        <div class="nav-fostrap">
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="services.html">Services<span class="arrow-down"></span></a>
-                    <ul class="dropdown">
-                        <li><a href="captive-services.html">Captive Services</a></li>
-                        <li><a href="insurance-companies.html">Insurance Companies</a></li>
-                        <li><a href="regulatory-services.html">Regulatory Services</a></li>
-                    </ul>
-                </li>
-                <li><a href="team.html">Our Team</a></li>
-                <li><a class="active" href="contact.php">Contact Us</a></li>
-            </ul>
-        </div>
-        <div class="nav-bg-fostrap">
-            <div class="navbar-fostrap"> <span></span> <span></span> <span></span> </div>
-            <a href="" class="title-mobile"></a>
-        </div>
-    </nav>
-
-  <!-----------------------------Main Content------------------------------------------->
-  <main class="wrapper">
-    <div class="left-wrapper">
-        <div class="left-content">
-            <img src="images/image16.jpg" alt="Sample Image 16">
-        </div>
-    </div>
-    < class="right-wrapper">
-        <article class="right-content">
-
-            <h1>Contact Us</h1>
-            <hr class="style1">
-
-            <p><b>Questions?</b></p>
-            <p class="underscore">We are happy to answer any questions or concerns.</p>
-
-            <?=$thankYou ?>
-
-            <form method="post" action="contact.php">
-                <label>Name:</label>
-                <input name="sender">
-        
-                <label>Email address:</label>
-                <input name="senderEmail">
-        
-                <label>Message:</label>
-                <textarea rows="5" cols="20" name="message"></textarea>
-        
-                <input type="submit" name="submit">
-            </form>
-                  
-        </article>
-    </div>
-</main>
-
-<!-----------------------------Page Footer------------------------------------------->
-    <footer id="full-width-bottom">
-        <div class="row">
-            <div class="column 1">
-                &copy;&nbsp;<span id="year"></span>&nbsp;&nbsp;Taylor-Walker Consulting, LLC
-            </div>
-            <div class="column 2">
-                10619 So. Jordan Gateway, Suite 335
-            </div>
-            <div class="column 3">
-                South Jordan, Utah 84095
-            </div>
-            <div class="column 4">
-                Telephone: (801) 562-5748
-            </div>
-        </div>
-    </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="js/toggle.js"></script>
-    <script src="js/current_date.js"></script>
+    $mail = new PHPMailer;
+    
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+    $mail->addReplyTo($from);
+    
+    $mail->isHTML(true);
+    
+    $mail->Subject = $subject;
+    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+    
+    
+    if(!$mail->send()) {
+        throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+    }
+    
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
 
 
-</body>
-
-</html>
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
