df0589c65f4a02e3b037c65a6fbdd77d9ac1868c
jcgolson <juanita.golson@gmail.com>
2020-03-27 16:16:54 -0500

    commit

--- contact.php
+++ contact.php
@@ -2,12 +2,16 @@
 // require ReCaptcha class
 require('recaptcha-master/src/autoload.php');
 
-// configure
+require('PHPMailer-master/PHPMailerAutoload.php');
+
+
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <juanita.golson@gmail.com>';
+$fromEmail = 'demo@domain.com';
+$fromName = 'Demo contact form';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Juanita Golson <juanita.golson@gmail.com>';
+$sendToEmail = 'demo@domain.com';
+$sendToName = 'Demo contact form';
 
 // subject of the email
 $subject = 'New message from contact form';
@@ -17,7 +21,7 @@ $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Contact form successfully submitted. Thank you, Taylor-Walker Consulting, LLC will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
@@ -78,7 +82,7 @@ try {
 } catch (\Exception $e) {
     $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
 }
-
+/*
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
@@ -88,3 +92,43 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 } else {
     echo $responseArray['message'];
 }
+*/
+
+if ($responseArray['type'] == 'success') {
+    // success redirect
+
+    header('Location: email_sent.html');
+}
+else {
+    //error redirect
+    header('Location: email_error.html');
+}
+
+/* ----------- HTML letter formatting -----------------*/
+$emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
+$emailTextHtml .= "<table>";
+
+foreach ($_POST as $key => $value) {
+    // If the field exists in the $fields array, include it in the email
+    if (isset($fields[$key])) {
+        $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+    }
+}
+$emailTextHtml .= "</table><hr>";
+$emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Juanita</p>";
+
+$mail = new PHPMailer;
+
+$mail->setFrom($fromEmail, $fromName);
+$mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+$mail->addReplyTo($from);
+
+$mail->isHTML(true);
+
+$mail->Subject = $subject;
+$mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+
+if(!$mail->send()) {
+    throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+}
+
