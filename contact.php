58bf956ca8cb6c0244352a795b31b30560479c20
jcgolson <juanita.golson@gmail.com>
2020-05-20 09:21:22 -0500

    commit

--- contact.php
+++ contact.php
@@ -1,6 +1,9 @@
 <?php
 // require ReCaptcha class
-require('recaptcha-master/src/autoload.php');
+//require('recaptcha-master/src/autoload.php');
+
+
+
 
 // configure
 // an email address that will be in the From field of the email.
@@ -36,6 +39,25 @@ try {
         // validate the ReCaptcha, if something is wrong, we throw an Exception,
         // i.e. code stops executing and goes to catch() block
         
+        // Check if form was submitted:
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+
+        // Build POST request:
+        $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+        $recaptcha_secret = 'YOUR_RECAPTCHA_SECRET_KEY';
+        $recaptcha_response = $_POST['recaptcha_response'];
+    
+        // Make and decode POST request:
+        $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+        $recaptcha = json_decode($recaptcha);
+    
+        // Take action based on the score returned:
+        if ($recaptcha->score >= 0.5) {
+            // Verified - send email
+        } else {
+            // Not verified - show form error
+        }
+/*
         if (!isset($_POST['g-recaptcha-response'])) {
             throw new \Exception('ReCaptcha is not set.');
         }
@@ -51,7 +73,7 @@ try {
         if (!$response->isSuccess()) {
             throw new \Exception('ReCaptcha was not validated.');
         }
-        
+    */    
         // everything went well, we can compose the message, as usually
         
         $emailText = "You have a new message from your contact form\n\n========================================\n";
